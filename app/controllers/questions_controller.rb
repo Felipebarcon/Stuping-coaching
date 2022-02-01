e01db8d23ef43870d84c5dcedf4a50e5790360c7
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-01 20:56:42 +0100

    all ok

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,16 @@ class QuestionsController < ApplicationController
   def ask
     # TODO
   end
+
+  def answer
+    # TODO
+    @question = params[:question]
+    if params[:question] == "I am going to work"
+      @answers = "Great !"
+    elsif params[:question].include?("?")
+      @answers = "Silly question, get dressed and go to work!"
+    else
+      @answers = "I don't care, get dressed and go to work!"
+    end
+  end
 end
